38f67e8a01588b3d4765035af3b96878ebfc771b
JKdagan <javier.cadagan6@gmail.com>
2023-10-23 22:57:13 -0300

    Molde expendedor
    En vez de usar n depositos, crear un arrays de depositos que crea los depositos al construirse. PENDIENTE

---
+++ p2_tarea3/src/main/java/tarea3_logica/Expendedor.java
@@ -0,0 +1,14 @@
+package tarea3_logica;
+import java.util.ArrayList;
+import Monedas.*;
+import Productos.*;
+
+public class Expendedor {
+
+    private ArrayList<Deposito> depositos;
+
+    public Expendedor() {
+
+    }
+}
+
