3cec3940e8d7bf6c61e5545732bdd962404a2b3e
JavierTorresO <javitorres2022@udec.cl>
2023-11-02 19:21:43 -0300

    botones en el comprador

--- p2_tarea3/src/main/java/tarea3_GUI/PanelComprador.java
+++ p2_tarea3/src/main/java/tarea3_GUI/PanelComprador.java
@@ -10,12 +10,27 @@ public class PanelComprador extends JPanel {
     public PanelComprador(Color color){
         super();
         this.setBackground(color);
-        this.setSize(100,100);
+        this.setLayout(new BorderLayout());
         imagenComprador = new ImageIcon(getClass().getResource("/imagenes/Comprador.jpeg"));
         if (imagenComprador != null) {
             JLabel label = new JLabel(imagenComprador);
-            add(label);
+            add(label, BorderLayout.CENTER);
         }
+        JPanel botonesPanel = new JPanel();
+        botonesPanel.setLayout(new BoxLayout(botonesPanel, BoxLayout.PAGE_AXIS));
+
+        JButton boton1 = new JButton("Agregar Moneda de 100");
+        JButton boton2 = new JButton("Agregar Moneda de 500");
+        JButton boton3 = new JButton("Agregar Moneda de 1000");
+
+        botonesPanel.add(Box.createVerticalGlue());
+        botonesPanel.add(boton1);
+        botonesPanel.add(boton2);
+        botonesPanel.add(boton3);
+        botonesPanel.add(Box.createVerticalGlue());
+        //los glues es para tener espacio por arriba y por abajo
+
+        add(botonesPanel, BorderLayout.EAST);
     }
 
     @Override
