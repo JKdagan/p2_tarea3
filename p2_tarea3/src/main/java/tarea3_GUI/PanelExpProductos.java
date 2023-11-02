9f55b73690b308f70fea546872a4c979dfba2acf
JavierTorresO <javitorres2022@udec.cl>
2023-11-02 16:32:14 -0300

    panel de botones

--- p2_tarea3/src/main/java/tarea3_GUI/PanelExpProductos.java
+++ p2_tarea3/src/main/java/tarea3_GUI/PanelExpProductos.java
@@ -2,11 +2,40 @@ package tarea3_GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.ImageIcon;
 
 public class PanelExpProductos extends JPanel {
 
     public PanelExpProductos(Color color) {
         this.setBackground(color);
+
+        JPanel botonesPanel = new JPanel();
+        botonesPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JButton boton1 = new JButton("1. CocaCola");
+        JButton boton2 = new JButton("2. Sprite");
+        JButton boton3 = new JButton("3. Fanta");
+        JButton boton4 = new JButton("4. Super8");
+        JButton boton5 = new JButton("5. Snickers");
+
+        botonesPanel.add(boton1);
+        botonesPanel.add(boton2);
+        botonesPanel.add(boton3);
+        botonesPanel.add(boton4);
+        botonesPanel.add(boton5);
+
+        this.add(botonesPanel);
+
+
+
+
+
+
+
+
+
+
     }
-    //idea: mostrar una imagen de cada producto con un numero abajo que indique la cantidad y segun se entrega un producto se disminuye(un string)
+
+
 }
