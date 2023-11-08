94017ae8b04a0fbca8509045d4984927a04291f8
JavierTorresO <javitorres2022@udec.cl>
2023-11-08 01:47:34 -0300

    cambio en los botones de retirar

--- p2_tarea3/src/main/java/tarea3_GUI/PanelExpCompra.java
+++ p2_tarea3/src/main/java/tarea3_GUI/PanelExpCompra.java
@@ -25,14 +25,13 @@ public class PanelExpCompra extends JPanel {
         boton_retirar_producto = new BotonRetirarProducto("Retirar producto");
         boton_retirar_vuelto = new BotonRetirarVuelto("Retirar vuelto de a 100");
         boton_rellenar=new BotonRellenarDepositos("Rellenar depositos");
-        JPanel panelbotonesRetirar=new JPanel();
-        panelbotonesRetirar.add(boton_retirar_producto);
-        panelbotonesRetirar.add(boton_retirar_vuelto);
+
 
 
         panel = new JPanel();
         panel.setLayout(new BorderLayout());
-        panel.add(panelbotonesRetirar,BorderLayout.NORTH);
+        panel.add(boton_retirar_producto,BorderLayout.EAST);
+        panel.add(boton_retirar_vuelto,BorderLayout.WEST);
         panel.add(boton_rellenar,BorderLayout.SOUTH);
         panel.add(labelImagen,BorderLayout.CENTER);
 
@@ -42,26 +41,12 @@ public class PanelExpCompra extends JPanel {
     public static void actualizarImagen(ImageIcon imagen) {
         if (imagen != null) {
             Image img = imagen.getImage();
-            Image newImg = img.getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+            Image newImg = img.getScaledInstance(280, 130, Image.SCALE_SMOOTH);
             imagen = new ImageIcon(newImg);
         }
         labelImagen.setIcon(imagen);
     }
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
 
-        int width = getWidth();
-        int height = getHeight();
-
-        g.setColor(Color.gray);
-        int rectWidth = 500; // Ancho del rectángulo
-        int rectHeight = 130; // Alto del rectángulo
-        int x = (width - rectWidth) / 2;
-        int y = (height - rectHeight) / 2;
-        g.fillRect(x, y, rectWidth, rectHeight);
-        labelImagen.setBounds(x-65,y-35,280,140);
-    }
     private class BotonRetirarProducto extends JButton {
         public BotonRetirarProducto(String texto) {
             super(texto);
