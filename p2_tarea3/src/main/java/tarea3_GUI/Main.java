1102e30910f4b4676532e757ba1d872dcffe3582
JavierTorresO <javitorres2022@udec.cl>
2023-11-08 20:44:07 -0300

    documentar

--- p2_tarea3/src/main/java/tarea3_GUI/Main.java
+++ p2_tarea3/src/main/java/tarea3_GUI/Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        //Creamos la Ventana para que se pueda ver el programa con los paneles y botones
         Ventana ventana= new Ventana();
     }
 }
